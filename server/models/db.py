b835989f5fb61a2b490ace39af62006de03e24c8
datesann0109 <t118053.kushima@gmail.com>
2023-10-29 07:18:48 +0900

    余分なライブラリりを削除

--- server/models/db.py
+++ server/models/db.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-DB_URL = "mysql+aiomysql://user:password@db:3306/jphacks?charset=utf8"
-             
+DB_URL = "mysql://user:password@db:3306/jphacks?charset=utf8"
+
 engine = create_engine(DB_URL)
 Base = declarative_base()
 session = scoped_session(sessionmaker(bind=engine))
