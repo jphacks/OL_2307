9ea82150c8f30e4757149355998ac10552337d17
datesann0109 <t118053.kushima@gmail.com>
2023-10-29 07:11:45 +0900

    cors対策

--- server/main.py
+++ server/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
 
 from routers import chatroom, talkroom, send_select
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(talkroom.router)
 app.include_router(chatroom.router)
 app.include_router(send_select.router)
